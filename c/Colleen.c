f45d9ab3d6f4255027dc55a23071b52112127212
Sarah Franc <shfranc@gmail.com>
2019-07-09 00:01:18 +0200

    feat(Colleen): C - wip

--- c/Colleen.c
+++ c/Colleen.c
@@ -1,4 +1,32 @@
+/* Colleen */
+#include <stdio.h>
+#define NL 10
+#define	QUOTE 34
+
+void print(char *code)
+{
+	/* print source code */
+	printf(code, QUOTE, code, QUOTE);
+}
+
 int	main()
 {
+	char *code = "#include <stdio.h>\n\
+#define NL 10\n\
+#define	QUOTE 34\n\
+\n\
+void print(char *code)\n\
+{\n\
+	/* print source code */\n\
+	printf(code, QUOTE, code, QUOTE);\n\
+}\n\
+\n\
+int	main()\n\
+{\n\
+	char	*code = %c%s%c;\n\
+	print(code);\n\
+	return 0;\n\
+}\n";
+	print(code);
 	return 0;
 }
