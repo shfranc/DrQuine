386ee1ed73c3f63717ff2b03b9ef3eb43d5d7697
Sarah Franc <shfranc@gmail.com>
2019-07-12 01:40:10 +0200

    feat(Grace) - python fix open error

--- python/Grace.py
+++ python/Grace.py
@@ -1,4 +1,8 @@
-code = "code = {!r}\nwith open('Grace_kid.py', 'w') as f:\n\tf.write(code.format(code))\n\tf.close()"
-with open('Grace_kid.py', 'w') as f:
-	f.write(code.format(code))
-	f.close()
+import sys
+code = "import sys\ncode = {!r}\ntry:\n    with open('Grace_kid.py', 'w') as f:\n        f.write(code.format(code))\n        f.close()\nexcept:\n    sys.exit()"
+try:
+    with open('Grace_kid.py', 'w') as f:
+        f.write(code.format(code))
+        f.close()
+except:
+    sys.exit()
