4975feea918a3b38b556e63a3d2451746b7dcbf8
Sarah Franc <shfranc@gmail.com>
2019-07-12 12:04:18 +0200

    fix(Colleen): python - add main

--- python/Colleen.py
+++ python/Colleen.py
@@ -1,2 +1,8 @@
-code = 'code = {0!r}\nprint(code.format(code))'
-print(code.format(code))
+# Colleen
+def main():
+    # print source code
+    code = '# Colleen\ndef main():\n    # print source code\n    code = {0!r}\n    print(code.format(code))\n\nif __name__ == "__main__":\n    main()'
+    print(code.format(code))
+
+if __name__ == "__main__":
+    main()
