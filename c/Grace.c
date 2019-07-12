18bd0ecf6bbf313d1c0d970d2f7358c302dbfdc3
Sarah Franc <shfranc@gmail.com>
2019-07-12 11:49:31 +0200

    fix(Grace): close fd

--- c/Grace.c
+++ c/Grace.c
@@ -1,11 +1,13 @@
 /* Grace */
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 #define NEWLINE 10
 #define QUOTE 34
 #define start main
 int start()
-{ char *code = "/* Grace */%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#define NEWLINE 10%1$c#define QUOTE 34%1$c#define start main%1$cint start()%1$c{ char *code = %2$c%s%2$c;%1$cint fd = open(%2$cGrace_kid.c%2$c, O_RDWR | O_CREAT, 00644); if (fd < 0) { return 0; }%1$cdprintf(fd, code, NEWLINE, QUOTE, code);%1$c}%1$c";
+{ char *code = "/* Grace */%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#define NEWLINE 10%1$c#define QUOTE 34%1$c#define start main%1$cint start()%1$c{ char *code = %2$c%s%2$c;%1$cint fd = open(%2$cGrace_kid.c%2$c, O_RDWR | O_CREAT, 00644); if (fd < 0) { return 0; }%1$cdprintf(fd, code, NEWLINE, QUOTE, code);%1$cclose(fd);%1$c}%1$c";
 int fd = open("Grace_kid.c", O_RDWR | O_CREAT, 00644); if (fd < 0) { return 0; }
 dprintf(fd, code, NEWLINE, QUOTE, code);
+close(fd);
 }
