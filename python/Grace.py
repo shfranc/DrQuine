c07ef005ab444a413736c4c4c11411665f579295
Sarah Franc <shfranc@gmail.com>
2019-07-26 15:16:50 +0200

    fix(Grace): python - add comment

--- python/Grace.py
+++ python/Grace.py
@@ -1,5 +1,6 @@
+# Grace
 import sys
-code = "import sys\ncode = {!r}\ntry:\n    with open('Grace_kid.py', 'w') as f:\n        f.write(code.format(code))\n        f.close()\nexcept:\n    sys.exit()"
+code = "# Grace\nimport sys\ncode = {!r}\ntry:\n    with open('Grace_kid.py', 'w') as f:\n        f.write(code.format(code))\n        f.close()\nexcept:\n    sys.exit()"
 try:
     with open('Grace_kid.py', 'w') as f:
         f.write(code.format(code))
