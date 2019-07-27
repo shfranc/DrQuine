65d64be2b24547c2d2940776e17b31ba56ed38cf
Sarah Franc <shfranc@gmail.com>
2019-07-27 20:13:29 +0200

    fix(Grace): asm - use a macro to replace the main

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,28 +1,20 @@
 ; Grace
-%define begin _main
 %define OPEN 0x2000005
 %define CLOSE 0x2000006
-section .data
-file_name: db "Grace_kid.s", 0
-code: db "; Grace%2$c%4$cdefine begin _main%2$c%4$cdefine OPEN 0x2000005%2$c%4$cdefine CLOSE 0x2000006%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$cglobal begin%2$cextern _dprintf%2$c%2$cstart:%2$ccall begin%2$cret%2$c%2$cbegin:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, 0x0201%2$cmov rdx, 0x1A4%2$cmov rax, OPEN%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, CLOSE%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c", 0
+%define FLAGS 03001o
 
-section .text
-global start
-global begin
+%macro FT 2
+global _main
 extern _dprintf
 
-start:
-call begin
-ret
-
-begin:
+_main:
 push rbp
 mov rbp, rsp
 sub rsp, 16
 
 lea rdi, [rel file_name]
-mov rsi, 0x0201
-mov rdx, 0x1A4
+mov rsi, FLAGS
+mov rdx, 0644o
 mov rax, OPEN
 syscall
 jc .leave
@@ -43,3 +35,16 @@ syscall
 .leave:
 leave
 ret
+%endmacro
+
+section .data
+file_name: db "Grace_kid.s", 0
+code: db "; Grace%2$c%4$cdefine OPEN 0x2000005%2$c%4$cdefine CLOSE 0x2000006%2$c%4$cdefine FLAGS 03001o%2$c%2$c%4$cmacro FT 2%2$cglobal _main%2$cextern _dprintf%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, FLAGS%2$cmov rdx, 0644o%2$cmov rax, OPEN%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, CLOSE%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c%4$cendmacro%2$c%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$c%2$cstart:%2$cFT file_name, code%2$cret%2$c%2$c", 0
+
+section .text
+global start
+
+start:
+FT file_name, code
+ret
+
