9e9f04d4f6aefb86853e7eafccaeaa416bafa987
Sarah Franc <shfranc@gmail.com>
2019-07-27 14:53:11 +0200

    fix(Grace): no main + add 3 defines

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,18 +1,21 @@
 ; Grace
+%define begin _main
+%define OPEN 0x2000005
+%define CLOSE 0x2000006
 section .data
 file_name: db "Grace_kid.s", 0
-code: db "; Grace%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$cglobal _main%2$cextern _dprintf%2$c%2$cstart:%2$ccall _main%2$cret%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, 0x0201%2$cmov rdx, 0x1A4%2$cmov rax, 0x2000005%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, 0x2000006%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c", 0
+code: db "; Grace%2$c%4$cdefine begin _main%2$c%4$cdefine OPEN 0x2000005%2$c%4$cdefine CLOSE 0x2000006%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$cglobal begin%2$cextern _dprintf%2$c%2$cstart:%2$ccall begin%2$cret%2$c%2$cbegin:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, 0x0201%2$cmov rdx, 0x1A4%2$cmov rax, OPEN%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, CLOSE%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c", 0
 
 section .text
 global start
-global _main
+global begin
 extern _dprintf
 
 start:
-call _main
+call begin
 ret
 
-_main:
+begin:
 push rbp
 mov rbp, rsp
 sub rsp, 16
@@ -20,7 +23,7 @@ sub rsp, 16
 lea rdi, [rel file_name]
 mov rsi, 0x0201
 mov rdx, 0x1A4
-mov rax, 0x2000005
+mov rax, OPEN
 syscall
 jc .leave
 mov [rsp], rax
@@ -30,10 +33,11 @@ lea rsi, [rel code]
 lea rdx, [rel code]
 mov rcx, `\n`
 mov r8, `"`
+mov r9, `%`
 call _dprintf
 
 mov rdi, [rsp]
-mov rax, 0x2000006
+mov rax, CLOSE
 syscall
 
 .leave:
