f9abcb304c4b856c0a36bf12e500befd3b1676a1
Sarah Franc <shfranc@gmail.com>
2019-07-27 22:06:19 +0200

    feat(Sully): asm - wip

--- asm/Grace.s
+++ asm/Grace.s
@@ -39,7 +39,7 @@ ret
 
 section .data
 file_name: db "Grace_kid.s", 0
-code: db "; Grace%2$c%4$cdefine OPEN 0x2000005%2$c%4$cdefine CLOSE 0x2000006%2$c%4$cdefine FLAGS 03001o%2$c%2$c%4$cmacro FT 2%2$cglobal _main%2$cextern _dprintf%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, FLAGS%2$cmov rdx, 0644o%2$cmov rax, OPEN%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, CLOSE%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c%4$cendmacro%2$c%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$c%2$cstart:%2$cFT file_name, code%2$cret%2$c%2$c", 0
+code: db "; Grace%2$c%4$cdefine OPEN 0x2000005%2$c%4$cdefine CLOSE 0x2000006%2$c%4$cdefine FLAGS 03001o%2$c%2$c%4$cmacro FT 2%2$cglobal _main%2$cextern _dprintf%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c%2$clea rdi, [rel file_name]%2$cmov rsi, FLAGS%2$cmov rdx, 0644o%2$cmov rax, OPEN%2$csyscall%2$cjc .leave%2$cmov [rsp], rax%2$c%2$cmov rdi, [rsp]%2$clea rsi, [rel code]%2$clea rdx, [rel code]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$ccall _dprintf%2$c%2$cmov rdi, [rsp]%2$cmov rax, CLOSE%2$csyscall%2$c%2$c.leave:%2$cleave%2$cret%2$c%4$cendmacro%2$c%2$csection .data%2$cfile_name: db %3$cGrace_kid.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$c%2$csection .text%2$cglobal start%2$c%2$cstart:%2$cFT file_name, code%2$cret%2$c", 0
 
 section .text
 global start
@@ -47,4 +47,3 @@ global start
 start:
 FT file_name, code
 ret
-
--- asm/Sully.s
+++ asm/Sully.s
@@ -1,9 +1,43 @@
+%macro write_file 3
+lea rdi, [rel file_name]
+mov rsi, 03001o
+mov rdx, 0644o
+mov rax, 0x2000005
+syscall
+jc .leave
+mov [rsp + 4], rax
+
+mov rdi, [rsp + 4]
+lea rsi, [rel code]
+lea rdx, [rel code]
+mov rcx, `\n`
+mov r8, `"`
+mov r9, `%`
+mov r10, index
+call _dprintf
+
+mov rdi, [rsp + 4]
+mov rax, 0x2000006
+syscall
+%endmacro
+
 section .data
+index: equ 5
+index_1: equ index - 1
+name_format: db "Sully_%d.s", 0
+code: db "", 0
+compil: db "nasm -f macho64 Sully_%1$d.s -o objs/Sully_%1$d.o && ld -macosx_version_min 10.8 -lSystem objs/Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d", 0
+
+section .bss
+file_name: resb 20
+cmd: resb 150
 
 section .text
 global start
 global _main
-global _sprintf
+extern _sprintf
+extern _dprintf
+extern _system
 
 start:
 call _main
@@ -12,6 +46,30 @@ ret
 _main:
 push rbp
 mov rbp, rsp
-sub rsp, 16
+sub rsp, 64
+; write itself
+mov rdi, file_name
+mov rsi, name_format
+mov rdx, index
+call _sprintf
+write_file file_name, code, index
+; if index is not 0
+mov qword[rsp], index
+cmp qword[rsp], 0
+je .leave
+; write a son
+mov rdi, file_name
+mov rsi, name_format
+mov rdx, index_1
+call _sprintf
+write_file file_name, code, index_1
+; and execute son
+mov rdi, cmd
+mov rsi, compil
+mov rdx, index_1
+call _sprintf
+mov rdi, cmd
+call _system
+.leave:
 leave
 ret
