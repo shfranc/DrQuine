9472060e4e5e37dff7de7c7c72db103ced91a7b3
Sarah Franc <shfranc@gmail.com>
2019-07-28 00:01:14 +0200

    feat(Sully): asm - done by compiling itself first

--- asm/Sully.s
+++ asm/Sully.s
@@ -21,12 +21,22 @@ mov rax, 0x2000006
 syscall
 %endmacro
 
+%macro execve 2
+mov rdi, cmd
+mov rsi, %1
+mov rdx, %2
+call _sprintf
+mov rdi, cmd
+call _system
+%endmacro
+
 section .data
 index: equ 5
 index_1: equ index - 1
 name_format: db "Sully_%d.s", 0
-code: db "%4$cmacro write_file 3%2$clea rdi, [rel %4$c1]%2$cmov rsi, 03001o%2$cmov rdx, 0644o%2$cmov rax, 0x2000005%2$csyscall%2$cjc .leave%2$cmov [rsp + 4], rax%2$c%2$cmov rdi, [rsp + 4]%2$clea rsi, [rel %4$c2]%2$clea rdx, [rel %4$c2]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$cmov qword[rsp], %4$c3%2$ccall _dprintf%2$c%2$cmov rdi, [rsp + 4]%2$cmov rax, 0x2000006%2$csyscall%2$c%4$cendmacro%2$c%2$csection .data%2$cindex: equ %5$d%2$cindex_1: equ index - 1%2$cname_format: db %3$cSully_%4$cd.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$ccompil: db %3$cnasm -f macho64 Sully_%4$c1$d.s -o objs/Sully_%4$c1$d.o && ld -macosx_version_min 10.8 -lSystem objs/Sully_%4$c1$d.o -o Sully_%4$c1$d && ./Sully_%4$c1$d%3$c, 0%2$c%2$csection .bss%2$cfile_name: resb 20%2$ccmd: resb 150%2$c%2$csection .text%2$cglobal start%2$cglobal _main%2$cextern _sprintf%2$cextern _dprintf%2$cextern _system%2$c%2$cstart:%2$ccall _main%2$cret%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 64%2$c; write itself%2$cmov rdi, file_name%2$cmov rsi, name_format%2$cmov rdx, index%2$ccall _sprintf%2$cwrite_file file_name, code, index%2$c; if index is not 0%2$cmov qword[rsp], index%2$ccmp qword[rsp], 0%2$cje .leave%2$c; write a son%2$cmov rdi, file_name%2$cmov rsi, name_format%2$cmov rdx, index_1%2$ccall _sprintf%2$cwrite_file file_name, code, index_1%2$c; and execute son%2$cmov rdi, cmd%2$cmov rsi, compil%2$cmov rdx, index_1%2$ccall _sprintf%2$cmov rdi, cmd%2$ccall _system%2$c.leave:%2$cleave%2$cret%2$c", 0
-compil: db "nasm -f macho64 Sully_%1$d.s -o objs/Sully_%1$d.o && ld -macosx_version_min 10.8 -lSystem objs/Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d", 0
+code: db "%4$cmacro write_file 3%2$clea rdi, [rel %4$c1]%2$cmov rsi, 03001o%2$cmov rdx, 0644o%2$cmov rax, 0x2000005%2$csyscall%2$cjc .leave%2$cmov [rsp + 4], rax%2$c%2$cmov rdi, [rsp + 4]%2$clea rsi, [rel %4$c2]%2$clea rdx, [rel %4$c2]%2$cmov rcx, `\n`%2$cmov r8, `%3$c`%2$cmov r9, `%4$c`%2$cmov qword[rsp], %4$c3%2$ccall _dprintf%2$c%2$cmov rdi, [rsp + 4]%2$cmov rax, 0x2000006%2$csyscall%2$c%4$cendmacro%2$c%2$c%4$cmacro execve 2%2$cmov rdi, cmd%2$cmov rsi, %4$c1%2$cmov rdx, %4$c2%2$ccall _sprintf%2$cmov rdi, cmd%2$ccall _system%2$c%4$cendmacro%2$c%2$csection .data%2$cindex: equ %5$d%2$cindex_1: equ index - 1%2$cname_format: db %3$cSully_%4$cd.s%3$c, 0%2$ccode: db %3$c%1$s%3$c, 0%2$ccompil: db %3$cnasm -f macho64 Sully_%4$c1$d.s -o objs/Sully_%4$c1$d.o && ld -macosx_version_min 10.8 -lSystem objs/Sully_%4$c1$d.o -o Sully_%4$c1$d%3$c, 0%2$cexec_bin: db %3$c./Sully_%4$c1$d%3$c, 0%2$c%2$csection .bss%2$cfile_name: resb 20%2$ccmd: resb 150%2$c%2$csection .text%2$cglobal start%2$cglobal _main%2$cextern _sprintf%2$cextern _dprintf%2$cextern _system%2$c%2$cstart:%2$ccall _main%2$cret%2$c%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$c; write itself%2$cmov rdi, file_name%2$cmov rsi, name_format%2$cmov rdx, index%2$ccall _sprintf%2$cwrite_file file_name, code, index%2$c; compile itself%2$cexecve compil, index%2$c; if index is not 0%2$cmov qword[rsp], index%2$ccmp qword[rsp], 0%2$cje .leave%2$c; write a son%2$cmov rdi, file_name%2$cmov rsi, name_format%2$cmov rdx, index_1%2$ccall _sprintf%2$cwrite_file file_name, code, index_1%2$c; compile son%2$cexecve compil, index_1%2$c; execute son%2$cexecve exec_bin, index_1%2$c.leave:%2$cleave%2$cret%2$c", 0
+compil: db "nasm -f macho64 Sully_%1$d.s -o objs/Sully_%1$d.o && ld -macosx_version_min 10.8 -lSystem objs/Sully_%1$d.o -o Sully_%1$d", 0
+exec_bin: db "./Sully_%1$d", 0
 
 section .bss
 file_name: resb 20
@@ -46,13 +56,15 @@ ret
 _main:
 push rbp
 mov rbp, rsp
-sub rsp, 64
+sub rsp, 16
 ; write itself
 mov rdi, file_name
 mov rsi, name_format
 mov rdx, index
 call _sprintf
 write_file file_name, code, index
+; compile itself
+execve compil, index
 ; if index is not 0
 mov qword[rsp], index
 cmp qword[rsp], 0
@@ -63,13 +75,10 @@ mov rsi, name_format
 mov rdx, index_1
 call _sprintf
 write_file file_name, code, index_1
-; and execute son
-mov rdi, cmd
-mov rsi, compil
-mov rdx, index_1
-call _sprintf
-mov rdi, cmd
-call _system
+; compile son
+execve compil, index_1
+; execute son
+execve exec_bin, index_1
 .leave:
 leave
 ret
